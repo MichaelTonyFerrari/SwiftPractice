bfad0fa2a356a1cdc6d26b22409ec3b6ab216cf9
restores <ferraro2ndbase@gmail.com>
2017-09-22 19:31:33 -0400

    Array tricks

---
+++ Arrays.swift
@@ -0,0 +1,23 @@
+import UIkit 
+
+var arrayFloats = [Float]()
+var arrayFloats2: [Float] = [] 
+
+var placeHolder: [Float]
+var inferedArray = ["Uno", "Dos"]
+
+// Return count of array
+inferedArray.count
+// Check if array is empty 
+inferedArray.isEmpty 
+
+// Adding inserting and removing 
+
+// Replaced
+inferedArray[0] = "New"
+
+// Add to 
+inferedArray.append("Tres")
+
+// Specific location 
+inferedArray.insert("four", at: 3)
