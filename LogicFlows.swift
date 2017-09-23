2b6b74648733f55eff240eb164e9c5fa5098cdb0
restores <ferraro2ndbase@gmail.com>
2017-09-22 20:00:00 -0400

    If else statements

---
+++ LogicFlows.swift
@@ -0,0 +1,21 @@
+var myAge = 23 
+var drivingAge = 16
+
+if myAge >= drivingAge{
+	print("Get a license you fool")
+} else{
+	print("Sorry you need to grow up")
+}
+
+var firstCondition: Bool = true 
+var secondCondition = false 
+
+if firstCondition == true && secondCondition == true{
+	print("Continue")
+}
+	else if firstCondition{
+	print("Only first condition true")
+	}
+	else{
+	print("neither met")
+	}
