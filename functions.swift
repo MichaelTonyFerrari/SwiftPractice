4c2a05be9f841679041eb0d07b52268adecba9fb
restores <ferraro2ndbase@gmail.com>
2017-09-24 10:01:49 -0400

    complex functions, unwrapping and function calling other functions

--- functions.swift
+++ functions.swift
@@ -17,3 +17,38 @@ currentDateWithMessage(message: Mikey)
 
 var mReturn = currentDatewithReturn("Miikey")
 print(mReturn)
+
+// Complex functions 
+
+func currentdateWithOptional(message: String?){
+	if let text = message{
+		print("Its 9/24/17, \(text)")
+	}
+	else
+		print("Its 9/24/2017, but no message")
+}
+
+currentdateWithOptional(message: nil)
+currentdateWithOptional(message: "Mikey")
+
+func Alltogether(name: String, codinghours: Int, isTired: Bool? = true) -> String{
+	let output = "\(name) has been coding for \(codinghours). Tired yet? \(isTired!)"	//isTired! unwraps optional compard to isTired
+
+	return output
+}
+
+Alltogether(name: "Mikey", codinghours: 6, isTired: false)
+
+let output = Alltogether(name: "Mikey", codinghours: 6, isTired: false)
+print(output)
+
+func hello(name: String) -> String{
+	return "Hello \(name)"
+}
+
+func functionasparamater(hellofunc: (String) -> String, name: String){
+	
+	print(hello(name))
+}
+
+functionasparamater(hellofunc: hello, name: "mikey")
